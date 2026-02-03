395f3ae3d6df47e2f8b260db78b79366b3e5d49d
rtanjr07 <rtanjr07@gmail.com>
2026-02-04 00:28:52 +0800

    - Updating components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,6 @@ import { Hero } from "@/components/hero";
 import { OurTeam } from "@/components/our-team";
 import { WhoWeAre } from "@/components/who-we-are";
 import { OurServices } from "@/components/our-services";
-import { FeaturedGames } from "@/components/featured-games";
-import { LatestNews } from "@/components/latest-news";
-import { Community } from "@/components/community";
 import { Footer } from "@/components/SiteChrome/footer";
 
 export default function Home() {
